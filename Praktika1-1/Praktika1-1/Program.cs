fef5dd2069d045c44e8c876ed617f624b0b74771
ArunoVEVO <70853866+ArunoVEVO@users.noreply.github.com>
2020-09-21 23:22:22 +0300

    Add files via upload

---
+++ Praktika1-1/Praktika1-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Практика1
+{
+    class task1
+    {
+        struct Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+
+
+            public Person(string _firstName, string _lastName, int _age)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+            }
+
+            public override string ToString()
+            {
+                return firstName + " " + lastName + ", age " + age;
+            }
+        }
+        static void Main()
+        {
+            Person p = new Person("Tony", "Allen", 32);
+            Console.WriteLine(p);
+        }
+    }
+}
